58e8fa3d87a9028fa6e85dbbfcfaf3639ca6de04
alig2039 <alig2039@yahoo.com>
2022-04-28 16:50:43 +0000

    final state update

--- my-app/src/components/StatefulGreetingWithPrevState.js
+++ my-app/src/components/StatefulGreetingWithPrevState.js
@@ -12,23 +12,26 @@ class StatefulGreeting extends React.Component{
     }
 
     handleClick() {
-        this.setState({
-            introduction: "Goodbye!",
-            buttonText: 'Enter'
-          }, ()=>{
-              console.log('new state', this.state.introduction);
-              console.log('new state', this.state.buttonText);
-    } );
-    console.log(this.state.introduction);
-    console.log(this.state.buttonText);
-  }
+        this.setState((prevState, prevProps) => {
+          console.log("Previous State:", prevState);
+          console.log("Previous Props:", prevProps);
+          return {
+            introduction:
+              prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+            buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+          };
+        });
+      }
     
   increment() {
-    this.setState({
-        count: this.state.count + 1
-    })
-    console.log(this.state.count)
-}
+    this.setState((prevState, prevProps) => {
+      console.log("Previous State:", prevState);
+      console.log("Previous Props:", prevProps);
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  }
 
 incrementFive(){
     this.increment()
