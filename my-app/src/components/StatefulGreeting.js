2506929c8a1fb248d9c187d464e8b1466e5d2e74
alig2039 <alig2039@yahoo.com>
2022-04-28 15:56:31 +0000

    state functions

---
+++ my-app/src/components/StatefulGreeting.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+
+class StateFulGreeing extends React.Component { 
+    render(){
+        return <h1>Hello from the  stateful class component!</h1>
+    {
+    {
+
+
+export default StateFulGreeting;
