80ac5b01758103d596a222860a1c7b22ae838bfd
alig2039 <alig2039@yahoo.com>
2022-04-29 11:06:17 +0000

    rendering lists challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 import RenderingLists from "./components/RenderingLists";
 
 function App() {
   return <div className="App">
-     < RenderingLists/>
-    </div>;
+      <RenderingLists />
+  </div>;
 }
 
 export default App;
